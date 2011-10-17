54f5b4c40567d71d8f7e613f855be81c5e01b75e
Ayende Rahien <ayende@ayende.com>
2011-10-17 11:56:18 +0200

    Adding a way to create reservations

---
+++ CourseWarsaw/Controllers/ReservationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using CourseWarsaw.Models;
+using NHibernate.Linq;
+using System.Linq;
+
+namespace CourseWarsaw.Controllers
+{
+	public class ReservationController : NHibernateController
+	{
+		public ActionResult Find(string name, int count, DateTime from, DateTime to)
+		{
+			var tables = session.Query<Table>()
+				.Where(x => x.Occupancy >= count)
+				.Where(x => x.Reservations.Any(y => y.From >= from && y.To <= to) == false)
+				.Take(3)
+				.ToList();
+
+			return Json(tables.Select(x => new {x.Occupancy, x.Priority, x.Id}), JsonRequestBehavior.AllowGet);
+		}
+	}
+}
--- CourseWarsaw/Models/Table.cs
+++ CourseWarsaw/Models/Table.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace CourseWarsaw.Models
 {
 	public class Table
 	{
 		public virtual int Id { get; set; }
 		public virtual bool Priority { get; set; }
+		public virtual int Occupancy { get; set; }
+		public virtual ICollection<Reservation> Reservations { get; set; }
 	}
 }
