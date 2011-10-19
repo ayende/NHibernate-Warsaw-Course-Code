1c885ed63f1f083be30cc8c9a7219c8bd23e56c7
Boris Bucha <b.bucha@crif.com>
2011-10-19 15:05:06 +0200

    updates

--- CourseWarsaw/Infrastructure/HiddenPropertyAccessor.cs
+++ CourseWarsaw/Infrastructure/HiddenPropertyAccessor.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security.Principal;
+using CourseWarsaw.Models;
 using NHibernate;
 using NHibernate.Engine;
 using NHibernate.Event;
@@ -15,18 +16,43 @@ namespace CourseWarsaw.Infrastructure
 	{
 		public bool OnPreInsert(PreInsertEvent @event)
 		{
-			Set("CreatedAt", DateTime.Now, @event.Persister, @event.Entity, @event.State);
-			Set("CreatedBy", WindowsIdentity.GetCurrent().Name, @event.Persister, @event.Entity, @event.State);
-
+            if(@event.GetType() != typeof(Event))
+            {
+                Set("CreatedAt", DateTime.Now, @event.Persister, @event.Entity, @event.State);
+                Set("CreatedBy", WindowsIdentity.GetCurrent().Name, @event.Persister, @event.Entity, @event.State);
+            }
 			return false;
 		}
 
 		public bool OnPreUpdate(PreUpdateEvent @event)
 		{
-			Set("ModifiedAt", DateTime.Now, @event.Persister, @event.Entity, @event.State);
-			Set("ModifiedBy", WindowsIdentity.GetCurrent().Name, @event.Persister, @event.Entity, @event.State);
 
-			return false;
+
+            if (@event.GetType() != typeof(Event))
+            {
+                Set("ModifiedAt", DateTime.Now, @event.Persister, @event.Entity, @event.State);
+                Set("ModifiedBy", WindowsIdentity.GetCurrent().Name, @event.Persister, @event.Entity, @event.State);
+                
+                int i = 0;
+                foreach (var propName in @event.Persister.PropertyNames)
+                {
+                    if (@event.OldState[i] != @event.State[i])
+                    {
+                        var changeEvent = new Event
+                        {
+                            Message = string.Format("{0}#{1}.{2} at {3}",
+                                                    @event.Entity.GetType().Name,
+                                                    @event.Id,
+                                                    propName,
+                                                    DateTime.Now)
+                        };
+                        @event.Session.Save(changeEvent);
+                    }
+                    i++;
+                }
+            }
+
+		    return false;
 		}
 
 		private void Set(string name, object val, IEntityPersister entityPersister, object entity, object[] objects)
